deaa5732cf1f48f5b134d0998bf8e104fe82f01e
ferrari_enzo <fmardonkulov@gmail.com>
2017-11-23 23:38:51 +0600

    Правки

--- frontend/views/site/_blocks/block_content.php
+++ frontend/views/site/_blocks/block_content.php
@@ -1,8 +1,4 @@
 
-<div class="event-second">
-    <div class="row justify-content-end m-0">
-        <div class="ckeditor-2 ckeditor m-r-40">
-            <?=$block->text?>
-        </div>
-    </div>
+<div class="ckeditor">
+    <?=$block->text?>
 </div>
