7914ea2f1d902d97104aef83876eb2ae38da83c3
Черняев Алексей <cherniaev_a@tass.ru>
2017-12-18 15:39:11 +0300

    SA-1797 Init

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=<mysql.hostname>:<mysql.port>;dbname=<mysql.database>',
+            'username' => '<mysql.username>',
+            'password' => '<mysql.password>',
             'charset' => 'utf8',
         ],
         'mailer' => [
