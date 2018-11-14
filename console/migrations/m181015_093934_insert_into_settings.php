91551f31c2cf58d8947fa9b8d5c1f606bc7707b2
npyatak <pyatakn@gmail.com>
2018-11-14 18:47:05 +0300

    upd

--- console/migrations/m181015_093934_insert_into_settings.php
+++ console/migrations/m181015_093934_insert_into_settings.php
@@ -13,6 +13,35 @@ class m181015_093934_insert_into_settings extends Migration {
 
         $this->batchInsert('{{%settings}}', ['key', 'value', 'title', 'type'], [
             ['socials', '["fb","tw","ok","vk","tg"]', 'Соц.сети для "поделиться"', 3],
+            ['smi2_code', '
+                <h5>Новости партнеров</h5>
+                <div class="news-partners-els">
+                    <a class="news-partners-el" style="background-image:url(/images/more_events/Layer_86.jpg)">
+                        <div>
+                            <p>Запуск телескопа James Webb отложили до 2019 года</p>
+                            <span>12 апреля</span>
+                        </div>
+                    </a>
+                    <a class="news-partners-el" style="background-image:url(/images/more_events/Layer_86.jpg)">
+                        <div>
+                            <p>Запуск телескопа James Webb отложили до 2019 года</p>
+                            <span>12 апреля</span>
+                        </div>
+                    </a>
+                    <a class="news-partners-el" style="background-image:url(/images/more_events/Layer_86.jpg)">
+                        <div>
+                            <p>Запуск телескопа James Webb отложили до 2019 года</p>
+                            <span>12 апреля</span>
+                        </div>
+                    </a>
+                    <a class="news-partners-el" style="background-image:url(/images/more_events/Layer_86.jpg)">
+                        <div>
+                            <p>Запуск телескопа James Webb отложили до 2019 года</p>
+                            <span>12 апреля</span>
+                        </div>
+                    </a>
+                </div>
+            ', 'Код СМИ2', 2],
         ]);
     }
 
